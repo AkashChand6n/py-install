4cbd7f050d8fc20b55a4afb0950c01c81f030695
Akash Chandran <67275861+AkashChand6n@users.noreply.github.com>
2025-05-26 01:10:28 +0530

    Update pyinstall.groovy

--- vars/pyinstall.groovy
+++ vars/pyinstall.groovy
@@ -1,7 +1,9 @@
 def call() {
     sh '''
         sudo apt-get update -y
-        sudo apt-get install -y python3 python3-pip
-        python3 -m pip install --upgrade pip
+        sudo apt-get install -y python3 python3-pip python3-venv
+        python3 -m venv venv
+        . venv/bin/activate
+        pip install --upgrade pip
     '''
 }
